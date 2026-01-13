e870840a321fc9bf32557b6ab7613f681b127d15
KARTIKPAUL <arpilar401@gmail.com>
2026-01-13 17:15:37 +0530

    lecture 3 done

---
+++ Lecture_03/Assignment.md
@@ -0,0 +1,11 @@
+1.What is JSX ?
+Ans : JSX stands for JavaScript XML. It is a syntax extension for JavaScript often used with React to describe what the user interface should look like.
+
+2.SuperPower of JSX ?
+Ans : The "superpower" of JSX is not just that it looks like HTML; It's that it blurs the line between UI logic and UI structure. In tradition web development, we often have to jump back and forth between HTML file and a JavaScript file to make things happen. 
+
+3.Role of type attribute in script tag? What options can I use there?
+Ans : In modern web development, the type attribute on a script tag tells the browser how to interpret the contents of the script.
+The type attribute determines wheather the browser treats the code as a tradoitional classic script or a JavaScript Module(ESM). This changes how variables are scoped and how files are loaded.
+
+4.
---
+++ Lecture_03/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const Title = () => <h1 className="tite">This is Title</h1>;
+
+// const HeadingComponenent = () => (
+//   <div className="container">
+//     <Title />
+//     <h1>Namaste React Functional Component</h1>
+//   </div>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingComponenent />);
+
+// const parent = React.createElement("div", { id: "parent" , class : "title"}, [
+//   React.createElement("h1", { id: "heading1" }, "This is H1"),
+//   React.createElement("h2", { id: "heading2" }, "This is H2"),
+//   React.createElement("h3", { id: "heading3" }, "This is H3"),
+// ]);
+
+// const title = (
+//   <>
+//     <h1 className="heading1">This is H1</h1>
+//     <h2 className="heading1">This is H1</h2>
+//     <h3 className="heading1">This is H1</h3>
+//   </>
+// );
+
+
+
+
+
+// const Heading = () => {
+//   return(
+//     <h1>This is Heading</h1>
+//   )
+// }
+
+
+// const Title = () => {
+//   return(
+//     <>
+//     <Heading />
+//     <h1>This is H1</h1>
+//     <h2>This is H2</h2>
+//     <h3>This is H3</h3>
+//     </>
+//   )
+// }
+
+
+
+
+
+const FinalComponent = () => {
+  return(
+    <>
+    
+    </>
+  )
+}
+
+
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Title />);
