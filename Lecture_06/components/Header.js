9e36f3b8178bd824387c69563f7663582dbdf4d6
KARTIKPAUL <arpilar401@gmail.com>
2026-01-21 14:24:02 +0530

    big project swiggy project start with lecture 06

---
+++ Lecture_06/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="url" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
